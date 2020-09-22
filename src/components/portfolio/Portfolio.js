13a70097f55058f0e35b7dc44604a14effbe5768
Nitin Misra <nitin.misra84@gmail.com>
2020-09-22 10:34:38 -0700

    completed refactoring css

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -8,15 +8,15 @@ import "./Portfolio.css";
 export default function Portfolio() {
   return (
     <div id="outer-container">
+        <header className="navigation-bar">
+          <div className="name-text">Nitin Misra</div>
+          <HamMenu />
+        </header>
       <div className="portfolio-too-small">
         Your screen resolution is too small, please view my page on a larger
         screen for a better experience
       </div>
       <div id="inner-container">
-        <header className="navigation-bar">
-          <div className="name-text">Nitin Misra</div>
-          <HamMenu />
-        </header>
         <div id='components-container'>
           <Identity />
           <Skills />
