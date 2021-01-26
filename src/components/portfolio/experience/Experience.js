6fb96976797d0432b02dbb4337d4f555fb5f69f7
Nitin Misra <nitin.misra84@gmail.com>
2021-01-26 16:29:03 -0700

    formatting/added to experience

--- src/components/portfolio/experience/Experience.js
+++ src/components/portfolio/experience/Experience.js
@@ -5,6 +5,16 @@ export default function Experience() {
   return (
     <div id="experience-container">
       <h2>Experience</h2>
+      <div id="resume-btn">
+        <a
+          href="https://drive.google.com/open?id=0B9sXP0gSC3wtN0dLMGdfLXkzLXRyMS1MRTU4MzdDRjBZcFNr"
+          target="_blank"
+          className="print-resume-btn"
+          rel="noopener noreferrer"
+        >
+          Print
+        </a>
+      </div>
       <div id="cards-container">
         <div className="card">
           <h3 className="xp-position text">Web Development Mentor</h3>
@@ -15,16 +25,31 @@ export default function Experience() {
           </div>
           <ul>
             <li>
-              Taught multiple React stack technologies in a remote setting such
-              as React, JavaScript, SQL, Express, and Session to students to
-              best prepare them for a career in web development
+              Educate 100+ students around React stack technologies such as
+              React, Redux, SQL, Express, of which many have gone on to
+              successfully gain employment as a software engineer
             </li>
             <li>
-              Hosted daily scrum meetings with students in order to keep them on
-              track for graduation in a fast pace, bootcamp style curriculum
+              Implement cloud storage solutions using Amazon Web Services (S3)
+              which allows our users to upload their multimedia to be displayed
+              on the web app. This allowed the app to become dynamic to each
+              individual user
             </li>
             <li>
-              Created lesson plans that were tailored to the needs of the
+              Troubleshoot student’s code rapidly as student’s develop their own
+              personal projects using the stack we teach, React/Node/SQL
+            </li>
+            <li>
+              Manage daily scrum meetings with students in order to keep them on
+              track for graduation in a fast pace, bootcamp style curriculum and
+              practice Agile methodologies
+            </li>
+            <li>
+              Ensure student’s full stack projects and assignments met all
+              requirements through JEST
+            </li>
+            <li>
+              Create lesson plans that were tailored to the needs of the
               students
             </li>
           </ul>
@@ -36,6 +61,27 @@ export default function Experience() {
             <p className="xp-dates text">Sept 2019 - Dec 2019</p>
             <p className="xp-dates text">Phoenix, AZ</p>
           </div>
+          <ul>
+            <li>
+              Created a full stack satellite viewing app using React.js which
+              utilized NPM and included an Express server, JavaScript,
+              PostgreSQL, Bcrypt, Redux, and responsive design
+            </li>
+            <li>
+              Tested the backend server using tools such as Postman to ensure
+              API endpoints were RESTful and functioning to the design
+            </li>
+            <li>
+              Utilized AJAX/Axios calls to external APIs that converted a user’s
+              zip code to geo-graphical coordinates; then using that data
+              returned JSON about eh viewable times a specific satellite would
+              be viewable from the user’s specific location
+            </li>
+            <li>
+              Utilized Express-Session to send the users geographical location
+              to Redux, enabling it to be utilized in various components
+            </li>
+          </ul>
         </div>
         <div className="card">
           <h3 className="xp-position text">Program Manager</h3>
@@ -44,6 +90,30 @@ export default function Experience() {
             <p className="xp-dates text">Jan 2017 - Apr 2019</p>
             <p className="xp-dates text">Phoenix, AZ</p>
           </div>
+          <ul>
+            <li>
+              Partnered with Finance, Accounting, and Settlement to ensure end
+              to end requirements were met for every new customer onboarding
+            </li>
+            <li>
+              Trained partners on how to review their reconciliation data to
+              ensure every penny was accounted
+            </li>
+            <li>
+              Provided executive management with SLA reporting for period-end
+              metrics, which were used to present corporate health statistics to
+              the company’s stakeholders
+            </li>
+            <li>
+              Reconciled accounts whenever a partner discrepancy was brought to
+              the attention of Settlement Operations
+            </li>
+            <li>
+              Implemented new best practices to ensure down-time was kept to a
+              minimum in partner payments once their account was activated if an
+              error in the system occurred
+            </li>
+          </ul>
         </div>
         <div className="card">
           <h3 className="xp-position text">Assistant Program Manager</h3>
@@ -51,18 +121,38 @@ export default function Experience() {
           <div>
             <p className="xp-dates text">Feb 2012 - July 2016</p>
             <p className="xp-dates text">Los Angeles, CA</p>
-          </div>
+          </div>{" "}
+          <ul>
+            <li>
+              Defined, launched, and managed Consumer Networking related
+              products with new technologies and solutions for Global and
+              particularly North America, EMEA, and APAC operators and open
+              market channels which are still sold today such as the WUSB6100M
+            </li>
+            <li>
+              Completed competitive analysis of the marketplace in the Consumer
+              Networking space, creating product roadmaps based on current and
+              foreseeable market trends and communicating customer
+              insights/requirements to research and development
+            </li>
+            <li>
+              Worked closely with cross-functional teams through the concept,
+              development, pre-production builds, ramp-up production, and full
+              production phases ensuring products released on time and within
+              budget
+            </li>
+            <li>
+              Developed a system of business practices that resulted in faster
+              Time to Market product launches on a global scale without
+              sacrificing product quality. Variant products were able to launch
+              a full week sooner under these new practices
+            </li>
+            <li>
+              Utilized Oracle Agile PLM to create, maintain, and manage product
+              BOM and its lifecycle
+            </li>
+          </ul>
         </div>
-      </div>
-      <div id="resume-btn">
-        <a
-          href="https://drive.google.com/open?id=0B9sXP0gSC3wtN0dLMGdfLXkzLXRyMS1MRTU4MzdDRjBZcFNr"
-          target="_blank"
-          className="print-resume-btn"
-          rel="noopener noreferrer"
-        >
-          Printable Resume
-        </a>
       </div>
     </div>
   );
